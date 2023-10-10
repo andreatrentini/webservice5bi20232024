10afc5683a15cfd07fa3ace30af6e2aa93df2973
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2023-10-10 14:17:21 +0000

    Aggiunto script sql

---
+++ db-server/script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE gestione_ticket;
+
+USE gestione_ticket;
+
+CREATE TABLE Users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50),
+    cognome VARCHAR(50),
+    data_nascita DATE,
+    email VARCHAR(100),
+    username VARCHAR(20),
+    password VARCHAR(200)
+);
+
+INSERT INTO Users 
+(nome, cognome, data_nascita, email, username)
+VALUES ('Andrea', 'Trentini', '2023-04-08', 'andrea.trentini@marconirovereto.it', 'andrea.trentini');
